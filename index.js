d7f7e0c86e2eece3ccf84443e13e8b9b32b867f4
Anders Rune Jensen <arj03@protonmail.ch>
2019-07-29 23:28:10 +0200

    Remove double define

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var SecretStack = require('secret-stack')
 var caps = require('ssb-caps')
 var SSB = require('ssb-db')
-var caps = require('ssb-caps')
 
 //create a sbot with default caps. these can be overridden again when you call create.
 function createSsbServer () {
